8bfb2cd81e322f31b6852e80298a73ebaaca446b
Nicholas H <NicholasH7@hotmail.com>
2022-05-06 22:54:04 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,44 +1,30 @@
 import './index.js'
 import './App.css';
-//have to import useState from React. This is a 'hook' that will help us. I will explain more below.
 import {useState} from "react"
 
 function App() {
     
-    // Please do some research into React hooks. They are kind of the heart and soul of React. useState is the most basic
-    // Here we are defining a new state 'enteredText' and assigning it to an empty string ''. 
-    // We can then use 'setEnteredText' to modify the 'enteredText' state. 
+  
     const [enteredText, setEnteredText] = useState('')
     
-    // Here I am creating a new state that will handle capitalization for us... more on that below :) 
     const [caps, setCaps] = useState('')
     
-    // Here we create a 'valueClickHandler' that we will add to the button's onClick events
-    // onClick fires an event. We can take this event and get the button's value with e.target.value
-    // then we will use the setEnteredText function to combine two strings, the existing enteredText and the new e.target.value
-    // We also are handeling capitalization. Maybe it looks complicated but it's pretty straightforward
+   
     const valueClickHandler = (e) => {
-        // first we check if the shift key is activated. We also check if it's a letter that's been clicked
-        // because if it is not a letter, we don't care about capitalization.
-        // so here, if shift key is activated, we add the capital letter to the string, and then reset the caps
-        // state because with shift we only want one character to be capitalized
+  
         if (caps === 'shift' && e.target.value.toUpperCase() !== e.target.value.toLowerCase()) {
             setEnteredText(enteredText + e.target.value)
             setCaps('')
-        // Here we do a similar thing with capsLock, but instead of reseting the caps state, we let it persist,
-        // because if the user hits caps lock they want many characters to be capitalized
+        
         } else if (caps === 'capsLock' && e.target.value.toUpperCase() !== e.target.value.toLowerCase()) {
             setEnteredText(enteredText + e.target.value)
-        // and then this statement handles clicks if no capitalization is activated, or if the target value is not a letter
+        
         } else {
             setEnteredText(enteredText + e.target.value.toLowerCase())
         }
     }
 
-    // We need a seprate function for the backspace key. Here we are using the .slice js method on enteredText
-    // to remove the last character of our enteredText string. Note that we have to check if the last two characters
-    // are \n (this is how we use the enter button to create a new line, see below.) If it is \n on the end of the string, we
-    // have to slice off the last two characters instead of just one.
+  
     const backspaceClickHandler = (e) => {
         if (enteredText.slice(-2) === '\n') {
             setEnteredText(enteredText.slice(0, -2))
@@ -47,10 +33,7 @@ function App() {
         }
     }
 
-    // And here we need to create a seperate function and state for capitalization. 
-    // Basically, we want to know whether the 'shift' or 'capsLock' key has been touched. 
-    // We will take the target.value and set the caps state based on it. If the caps state is already active,
-    // we will remove caps state ('')
+  
     const capitalizeClickHandler = (e) => {
         if (caps === '') {
             setCaps(e.target.value)
@@ -63,9 +46,7 @@ function App() {
 
 <div className='keyboard-input'>
     
-    {/* Here we are making the text area render whatever the {enteredText} state is. 
-    I changed it to a <p> element instead of textarea, because the user can still type
-    in the textarea with their computers keyboard instead of using the virtual keyboard.*/}
+    
     <p class="use-keyboard-input">
         {enteredText}
     </p>
@@ -75,9 +56,7 @@ function App() {
         <div className = 'keyNumbers'>
         
 
-        {/* To make this work, we have to add some properties to the buttons. For each we add the value wich is just
-        a string of whatever the key is, and onClick which will fire the valueClickHandler we defined above. Also, we add the
-        backspaceClickHandler to the backspace button*/}
+        
         <button value={'1'} onClick={valueClickHandler}> 1 </button> 
         <button value={'2'} onClick={valueClickHandler}> 2 </button> 
         <button value={'3'} onClick={valueClickHandler}> 3 </button> 
@@ -108,8 +87,6 @@ function App() {
         <button value={'P'} onClick={valueClickHandler}> P </button> 
         <button value={'['} onClick={valueClickHandler}> [ </button> 
         <button value={']'} onClick={valueClickHandler}> ] </button> 
-        {/* Notice here we use two backslashes (\\). Check out: https://stackoverflow.com/questions/3903488/javascript-backslash-in-variables-is-causing-an-error  
-        for an explanation.*/}
         <button value={'\\'} onClick={valueClickHandler} className='span-twoo'> \ </button>
 
 
